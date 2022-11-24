92ac4dc73c92df7228852abc375c33028433293a
humbertodlacerda <humbertodlacerda@gmail.com>
2022-11-24 10:50:56 -0300

    implementation of classes 1 to 4

---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Project;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class ProjectsTest extends TestCase
+{
+    use WithFaker, WithoutMiddleware;
+
+    public function test_a_user_can_create_a_project()
+    {
+        $this->withoutExceptionHandling();
+
+        $atributes = [
+            'title' => fake()->sentence,
+            'description' => fake()->paragraph
+        ];
+
+        $this->post('/projects', $atributes)->assertRedirect('/projects');
+
+        $this->assertDatabaseHas('projects', $atributes);
+
+        $this->get('/projects')->assertSee($atributes['title']);
+    }
+
+    public function test_a_user_can_view_a_project()
+    {
+        $this->withoutExceptionHandling();
+
+
+        $project = Project::factory()->create();
+        
+        $this->get($project->path())
+            ->assertSee($project->title)
+            ->assertSee($project->description);
+
+    }
+    
+    public function test_a_project_requires_a_title()
+    {
+        $attributes = Project::factory()->raw(['title' => '']);
+
+        $this->post('projects', $attributes)->assertSessionHasErrors('title');
+    }
+
+    public function test_a_project_requires_a_description()
+    {
+        $attributes = Project::factory()->raw(['description' => '']);
+
+        $this->post('projects', $attributes)->assertSessionHasErrors('description');
+    }
+}
